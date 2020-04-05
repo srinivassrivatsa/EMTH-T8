1d46bda8d8c9d86529d1212fefff83d45966bb68
srinivassrivatsa <srinivas.srivatsa7@gmail.com>
2020-04-06 00:03:41 +0530

    dynamic card preview added to product form

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,12 +11,13 @@ import { ProductService } from 'src/app/product.service';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) { 
+  constructor(private router: Router, private categoryService: CategoryService, private productService: ProductService) { 
     this.categories$ = categoryService.getCategories().valueChanges();
   }
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['admin/products']);
   }
 
   ngOnInit(): void {
